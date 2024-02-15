1c3333837aff529dce81041217e1d6524dc147b4
Hirak <hirak170802@gmail.com>
2024-02-15 20:02:22 +0530

    model trainer added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from WineQualityPrediction import logger
 from WineQualityPrediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from WineQualityPrediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from WineQualityPrediction.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from WineQualityPrediction.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 STAGE_NAME="Data Ingestion stage"
-
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
@@ -11,3 +12,35 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME="Data Validation stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = ModelTrainerTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
---
+++ src/WineQualityPrediction/pipeline/stage_04_model_trainer.py
@@ -0,0 +1,26 @@
+from WineQualityPrediction.config.configuration import ConfigurationManager
+from WineQualityPrediction.components.model_trainer import ModelTrainer
+from WineQualityPrediction import logger
+
+
+STAGE_NAME = "Model Trainer Stage"
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>> stage {STAGE_NAME} started <<<<<<")
+        data_ingestion = ModelTrainerTrainingPipeline()
+        data_ingestion.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
